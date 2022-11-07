26112d79fe4e8f8ee1c71f0fb6c4b6a4824ee82e
Mariamakbbh <1711828@brunel.ac.uk>
2022-11-07 12:00:52 +0000

    fix: index file changes

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
       <Router>
         <Navbar/>
         <Routes>
-          <Route exact path='/' element = {<Home />}/>
+          <Route exact path='/mks_Portfolio' element = {<Home />}/>
           <Route exact path='/aboutme' element = {<AboutMe />}/>
         </Routes>
       </Router>
